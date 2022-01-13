8d13b74a2eba32cda6fece7165b12dd994f070e7
Joao Emmerich <emmerichjoao@protonmail.com>
2022-01-13 02:36:52 +0100

    fixed puppetter methods to run on linux environment and routes fixes for deployment

--- src/helpers/save-to-pdf.js
+++ src/helpers/save-to-pdf.js
@@ -2,15 +2,16 @@ const puppeteer = require("puppeteer");
 const path = require("path");
 
 
-const frontEndBase = "https//gordianknot.xyz";
+const frontEndBase = "https://gordianknot.xyz";
+
 const finalPath = path.join(__dirname, "../../client/output");
-const saveToPdf = async (id, name) => {
+const saveToPdf = async (id, name, token) => {
 
   try {
     
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const localStorage = {"ACCESS_TOKEN": token}
-setDomainLocalStorage(browser, frontEndBase, localStorage)    
+await setDomainLocalStorage(browser, frontEndBase, localStorage)    
 
     // old
 const page = await browser.newPage();
@@ -31,22 +32,28 @@ const page = await browser.newPage();
 };
 
 const setDomainLocalStorage = async (browser, url, values) => {
-  const page = await browser.newPage();
-  await page.setRequestInterception(true);
-  page.on('request', r => {
-    r.respond({
-      status: 200,
-      contentType: 'text/plain',
-      body: 'tweak me.',
+ try{
+
+   const page = await browser.newPage();
+   await page.setRequestInterception(true);
+   page.on('request', r => {
+     r.respond({
+       status: 200,
+       contentType: 'text/plain',
+       body: 'body',
+      });
     });
-  });
-  await page.goto(url);
-  await page.evaluate(values => {
-    for (const key in values) {
-      localStorage.setItem(key, values[key]);
-    }
-  }, values);
-  await page.close();
+    await page.goto(url, {waitUntil: "networkidle2"});
+    await page.evaluate(values => {
+      for (const key in values) {
+        localStorage.setItem(key, values[key]);
+      }
+    }, values);
+    await page.close();
+  } catch (error) {
+    console.log(error);
+    
+  } 
 };
 
 
